f14b8292ccdcf0c5fcc997b8cd9a49788bcb7303
Hernan Y.Ke <yuriqiao@gmail.com>
2016-06-12 22:52:12 -0400

    Create a modular web 1/8

--- my_app/hello/views.py
+++ my_app/hello/views.py
@@ -1,8 +1,21 @@
 __author__ = 'Hernan Y.Ke'
-from flask import render_template, request
+from flask import render_template, request,Blueprint
+from my_app.hello.models import MESSAGES
+
+hello = Blueprint('hello',__name__)
 
 @hello.route('/')
 @hello.route('/hello')
-def hello():
-    user = request.args.get('user', 'Shalabh')
-    return render_template('index.html', user=user)
+def hello_world():
+    return MESSAGES['default']
+
+
+@hello.route('/show/<key>')
+def get_message(key):
+    return MESSAGES.get(key) or "%s not found!" % key
+
+
+@hello.route('/add/<key>/<message>')
+def add_or_update_message(key, message):
+    MESSAGES[key] = message
+    return "%s Added/Updated" % key
